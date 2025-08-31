a3edac1552fd022df1d600e2108dff9b335e1019
WinnerWind <contact@winnerwind.in>
2025-08-31 11:18:08 +0530

    Audio fetching

--- fetchData.js
+++ fetchData.js
@@ -1,5 +1,7 @@
 const infoEl = document.getElementById("songInfo")
 const imageEl = document.getElementById("albumArt")
+const audioEl = document.getElementById("songAudioSrc")
+const audioParentEl = document.getElementById("songAudio")
 
 document.addEventListener('DOMContentLoaded', setDetails)
 
@@ -8,14 +10,15 @@ async function setDetails() {
 	let apiURL = "http://192.168.68.105:5001/music/"
 	let imagesAPI = apiURL + "images/"
 	let dataAPI = apiURL + "data/"
+	let audioAPI = apiURL + "audio/"
 	let url = window.location.href
 	let fileName = (url.split('/')).pop()
 
-	let data = await fetch(dataAPI+fileName, {method: 'POST'}).then(response => response.json()).
+	let data = await fetch(dataAPI+fileName).then(response => response.json()).
 	then(data => {
-		console.log(data)
 		infoEl.innerText = `${data.title} - ${data.album} - ${data.artists.join(", ")}`
-		console.log(apiURL+imagesAPI+fileName)
 		imageEl.src = imagesAPI+fileName
+		audioEl.src = audioAPI+fileName
+		audioParentEl.load()
 	})
 }
